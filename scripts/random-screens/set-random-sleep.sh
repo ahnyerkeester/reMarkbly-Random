86e0aa7e838c9619b49026d3c86c779bfb066d1e
ahnyerkeester <45885366+ahnyerkeester@users.noreply.github.com>
2025-03-19 08:45:38 -0700

    Correct set-random-sleep.sh script loaded
    Had the slightly modded script that just randomly slected pics from the folder. I'd made a better one that put them in random order and then moved through them. This is the more elaborate script.

--- scripts/random-screens/set-random-sleep.sh
+++ scripts/random-screens/set-random-sleep.sh
@@ -1,16 +1,67 @@
 #!/bin/sh
-# Branched from https://github.com/Neurone/reMarkable
-# This scrip will select a random pic from:
+#
+# set-random-sleep.sh with detection
+#
+# Branched and significantly expanded from
+# https://github.com/Neurone/reMarkable
+#
+# This scrip will randomly order the pics in:
 #   /home/root/customization/images/suspended
-#   and place it as the current sleep screen at:
-#   /usr/share/remarkable/suspended.png
+# Then, every time it runs, it checks to see if
+#  the images in the folder have changed. If not,
+#  it will select the next pic in order and set
+#  it as the current sleep screen at:
+#    /usr/share/remarkable/suspended.png
+#
+# However, if it detects a change in the folder,
+#  it will reindex them and then set the next one
+# as the current sleep screen.
 
 SUSPENDED_IMAGES_DIR=/home/root/customization/images/suspended
-SUSPENDED_FILE=/usr/share/remarkable/suspended.original.png
-if [ -d $SUSPENDED_IMAGES_DIR ]; then
-    SUSPENDED_IMAGES_COUNT=$(ls $SUSPENDED_IMAGES_DIR | wc -l)
-    if [ $SUSPENDED_IMAGES_COUNT -ne 0 ]; then
-        SUSPENDED_FILE=$(shuf -n1 -e $SUSPENDED_IMAGES_DIR/*)
-    fi;
+ORDER_FILE=/home/root/customization/.image_order
+INDEX_FILE=/home/root/customization/.image_index
+CHECKSUM_FILE=/home/root/customization/.image_checksum
+
+calculate_checksum() {
+    find "$SUSPENDED_IMAGES_DIR" -type f -exec md5sum {} + | sort -k 2 | md5sum | awk '{print $1}'
+}
+
+shuffle_images() {
+    ls "$SUSPENDED_IMAGES_DIR"/* > "$ORDER_FILE"
+    shuf "$ORDER_FILE" -o "$ORDER_FILE"
+    echo 0 > "$INDEX_FILE"
+    calculate_checksum > "$CHECKSUM_FILE"
+}
+
+update_image() {
+    TOTAL_IMAGES=$(wc -l < "$ORDER_FILE")
+    CURRENT_INDEX=$(cat "$INDEX_FILE")
+
+    if [ "$CURRENT_INDEX" -ge "$TOTAL_IMAGES" ]; then
+        shuffle_images
+        CURRENT_INDEX=0
+    fi
+
+    SUSPENDED_FILE=$(sed -n "$((CURRENT_INDEX + 1))p" "$ORDER_FILE")
+    cp "$SUSPENDED_FILE" /usr/share/remarkable/suspended.png
+
+    echo $((CURRENT_INDEX + 1)) > "$INDEX_FILE"
+}
+
+# Ensure the directory exists
+if [ ! -d "$SUSPENDED_IMAGES_DIR" ]; then
+    echo "Image directory does not exist. Exiting."
+    exit 1
 fi
-cp $SUSPENDED_FILE /usr/share/remarkable/suspended.png
+
+# Check for changes since last run
+NEW_CHECKSUM=$(calculate_checksum)
+if [ ! -f "$CHECKSUM_FILE" ] || [ "$NEW_CHECKSUM" != "$(cat $CHECKSUM_FILE)" ]; then
+    echo "Changes detected in the image directory, reindexing..."
+    shuffle_images
+else
+    echo "No changes detected. Using existing order."
+fi
+
+# Update image
+update_image
